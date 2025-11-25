dd977cbeecfe92c1d77123c1ffea24a970c17581
Paweł Daniluk <pawel.daniluk@kotalbert.net>
2025-11-25 09:04:45 +0100

    Add command handling stub

--- To-Do List (Python)/task/todolist/db.py
+++ To-Do List (Python)/task/todolist/db.py
@@ -3,5 +3,6 @@ import sqlite3
 
 def get_db():
     """Establishes a connection to the SQLite database and returns the connection object."""
+
     conn = sqlite3.connect('sqlite:///todo.db?check_same_thread=False')
     return conn
--- To-Do List (Python)/task/todolist/todolist.py
+++ To-Do List (Python)/task/todolist/todolist.py
@@ -1,7 +1,33 @@
-print("""
-Today:
-1) Do yoga
-2) Make a breakfast
-3) Learn the basics of SQL
-4) Learn about ORM
-""")
+"""Main module for the To-Do List application."""
+import sys
+
+
+def main():
+
+    while True:
+        show_menu()
+        handle_command()
+
+def handle_command() -> None:
+    """Handles user commands for the To-Do List application."""
+
+    command = input()
+    if command == '1':
+        print("Today's tasks")
+    elif command == '2':
+        print("Add a task")
+    elif command == '0':
+        print("Bye!")
+        sys.exit(0)
+    else:
+        print("Invalid option")
+
+
+def show_menu() -> None:
+    print("1) Today's tasks")
+    print("2) Add a task")
+    print("0) Exit")
+
+
+if __name__ == '__main__':
+    main()
